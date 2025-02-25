c860f6a06d110e86b7783cd0ad0ebfb06663b736
isaac armas <230156328@aston.ac.uk>
2025-02-25 01:12:42 +0000

    week4 1

--- lab4-primit/java/ErrorsEnum.java
+++ lab4-primit/java/ErrorsEnum.java
@@ -1,69 +1,60 @@
 import java.util.Scanner;
 import java.util.EnumSet;
 
-public class ErrorsEnum
-{
+public class ErrorsEnum {
     enum Error { FP_ROUNDING, FP_OVERFLOW, FP_UNDERFLOW, INT_OVERFLOW }
 
     enum Result { A_BIT_DIFFERENT, INFINITY, ZERO, VERY_DIFFERENT }
     
-    private static <E extends Enum<E>> E getEnumElement(String elementTypeName, Class<E> elementType)
-    {
+    private static <E extends Enum<E>> E getEnumElement(String elementTypeName, Class<E> elementType) {
         boolean haveResult = false;
         E result = null;
         Scanner stdin = new Scanner(System.in);
         
-        while ( ! haveResult )
-        {
+        while (!haveResult) {
             System.out.print("Input " + elementTypeName + ": ");
-            try
-            {
+            try {
                 result = Enum.valueOf(elementType, stdin.next().toUpperCase());
                 haveResult = true;
-            }
-            catch (IllegalArgumentException e)
-            {
+            } catch (IllegalArgumentException e) {
                 System.out.println("Not a valid " + elementTypeName + ".");
-                stdin.nextLine(); // skip the invalid input
+                stdin.nextLine(); // Skip the invalid input
             }
         }
         
         stdin.close();
         return result;
     }
-  
-    private static Result error2Result(Error e)
-    {
-        Result result = null;
+
+    private static Error result2Error(Result r) {
+        Error error = null;
         
-        switch (e) {
-        case FP_ROUNDING:
-            result = Result.A_BIT_DIFFERENT;
-            break;
-        case FP_OVERFLOW:
-            result = Result.INFINITY;
-            break;
-        case FP_UNDERFLOW:
-            result = Result.ZERO;
-            break;
-        case INT_OVERFLOW:
-            result = Result.VERY_DIFFERENT;
-            break;
+        switch (r) {
+            case A_BIT_DIFFERENT:
+                error = Error.FP_ROUNDING;
+                break;
+            case INFINITY:
+                error = Error.FP_OVERFLOW;
+                break;
+            case ZERO:
+                error = Error.FP_UNDERFLOW;
+                break;
+            case VERY_DIFFERENT:
+                error = Error.INT_OVERFLOW;
+                break;
         }
         
-        return result;
+        return error;
     }
 
-    public static void main(String[] args)
-    {
-        System.out.print("Known errors = ");
-        for (Error e : EnumSet.allOf(Error.class)) 
-        {
-            System.out.print(e + " ");
+    public static void main(String[] args) {
+        System.out.print("Known results = ");
+        for (Result r : EnumSet.allOf(Result.class)) {
+            System.out.print(r + " ");
         }
         System.out.println();
         
-        Error e = getEnumElement("error", Error.class);
-        System.out.println(e + " results in: " + error2Result(e));
+        Result r = getEnumElement("result", Result.class);
+        System.out.println(r + " is caused by: " + result2Error(r));
     }
 }
